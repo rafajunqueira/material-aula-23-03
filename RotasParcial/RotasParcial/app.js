fcce7c4e88b7160690395534674712626dfc7d83
rafajunqueira94 <rafael.junqueira@fatec.sp.gov.br>
2022-03-23 19:53:54 -0300

    material inicial

--- RotasParcial/RotasParcial/app.js
+++ RotasParcial/RotasParcial/app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const server = require('./config/server.js')
 
 http.createServer(server).listen(server.get('porta'), function() {
-    consoles.log('Servidor rodando na porta ' + server.get('porta'))
+    console.log('Servidor rodando na porta ' + server.get('porta'))
 })
 
 
